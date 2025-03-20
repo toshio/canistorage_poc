7a324d9d47ebd822d2e36d8480feb4218545f3f4
toshio <162767+toshio@users.noreply.github.com>
2025-03-21 00:41:52 +0900

    invoke method by specific principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,161 @@
 import { useState } from 'react';
+import { Actor } from "@dfinity/agent";
 import { init } from '@/utils/canistorage';
-import { CANISTORAGE_ID } from "@/const.ts";
+import { CANISTORAGE_ID, USERS } from "@/const.ts";
 
-let canistorage = null;
+let canistorage;
+
+// Canistorageのインスタンスを初期化
+(async ()=>{
+  console.log("init()");
+  canistorage = await init(CANISTORAGE_ID);
+})();
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [version, setVersion] = useState('');
+  // User
+  const [user, setUser] = useState('');
+  const [principal, setPrincipal] = useState('');
+  // listFiles
+  const [path, setPath] = useState('');
+  const [files, setFiles] = useState('');
+  const [errorListFiles, setErrorListFiles] = useState('');
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
+  function changeUser(e) {
+    const newUser = e.target.value;
+    setUser(newUser);
+    setPrincipal(USERS[newUser]?.principal || "");
 
-    if (!canistorage) {
-      canistorage = await init(CANISTORAGE_ID);
+    // Replace identity
+    if (newUser) {
+      const newIdentity = USERS[newUser]?.identity;
+      console.log(`newIdentity: ${newIdentity}`);
+
+      const agent = Actor.agentOf(canistorage);
+      agent.replaceIdentity(newIdentity);
     }
-    const version = await canistorage.version();
+  }
 
-    setGreeting(version);
+  async function getVersion() {
+    setVersion("");
+    try {
+      setVersion(await canistorage.version());
+    } catch (e) {
+      // do nothing
+    }
+  }
 
-    return false;
+  async function listFiles() {
+    setErrorListFiles("");
+    setFiles("");
+
+    try {
+      const result = await canistorage.listFiles(path);
+
+      if (result.Err) {
+        setErrorListFiles(result.Err.message);
+        setFiles("");
+      } else {
+        setErrorListFiles("");
+        setFiles(result.Ok);
+      }
+    } catch (e) {
+      setErrorListFiles("");
+      setErrorListFiles(e.message);
+    }
   }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-      <label htmlFor="name">Canister Id of your Canistorage:&nbsp;</label>
-        <input id="name" alt="CanistorageId" type="text" value={CANISTORAGE_ID} disabled/>
+      <h1>Canistorage (Proof of Concept)</h1>
+      <hr/>
+      <h2>Concept</h2>
+      <p>
+        Canistorageは、Internet Computerの仕組みを利用した分散型のクラウドストレージです。まだ開発の初期段階です。<br />
+        Internet Computer上でユーザーやアプリケーション（Canister）を識別するための『Principal』ベースでアクセス制御を行い、個人（や企業）のデータの記録・保管していくことを目指しています。<br />
+        これから先、数十年、数百年という長期にわたってデータを管理していくことを考えた場合に、ベンダーロックインされることなく、また、データの完全性・可用性の観点で、アクセスI/Fやデータ構造などの仕様がオープンに議論されて決定していくことが重要です。<br />
+      </p>
+      <h2>This site</h2>
+      <p>
+        この検証サイトは、Canistorageの動作を簡易確認するために用意したサンプルアプリケーションです。<br />
+      </p>
+      <hr/>
+      <h2>Interface</h2>
 
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      {/* col1: Name, col2: arguments, col3: button, col4: result */}
+      <table>
+        <tr>
+          <th colSpan="4">Access User</th>
+        </tr>
+        <tr>
+          <td>
+            <select id="user" onChange={changeUser}>
+              <option value=""></option>
+              {Object.keys(USERS).map((name, index) =>
+                <option value={name}>{name}</option>
+              )}
+            </select>
+         </td>
+         <td>&nbsp;</td>
+         <td>&nbsp;</td>
+         <td><input type="text" value={principal} readOnly /></td>
+        </tr>
+
+        <tr>
+          <th colSpan="4">Canistorage Information</th>
+        </tr>
+        <tr>
+          <td>Canister Id</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>
+            <input type="text" value={CANISTORAGE_ID} readOnly />
+          </td>
+        </tr>
+        <tr>
+          <td>Version</td>
+          <td>&nbsp;</td>
+          <td>
+            <button onClick={getVersion} type="submit" disabled={!user}>version()</button>
+          </td>
+          <td>
+            <input type="text" value={version} readOnly />
+          </td>
+        </tr>
+
+        <tr>
+          <td>listFiles</td>
+          <td>
+            <input id="listFilesPath" type="text" placeholder="Directory (Starts with '/')" onChange={(e)=>setPath(e.target.value)}/>
+          </td>
+          <td>
+            <button onClick={listFiles} type="submit" disabled={!user||!path}>listFile()</button>
+          </td>
+          <td>
+            <span className="error">{errorListFiles}</span>&nbsp;<br/>
+            <textarea defaultValue={files} readOnly />
+          </td>
+        </tr>
+      </table>
+      <hr/>
+      <h2>Permissions</h2>
+      <table>
+        <tr>
+          <th>Path</th>
+          <th>User</th>
+          <th>Principal</th>
+          <th>Readable</th>
+          <th>Writable</th>
+        </tr>
+        <tr>
+          <td>/</td>
+          <td>admin</td>
+          <td>{USERS["admin"]?.principal}</td>
+          <td>true</td>
+          <td>true</td>
+        </tr>
+      </table>
+
     </main>
   );
 }
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,20 @@
+import { Secp256k1KeyIdentity } from '@dfinity/identity-secp256k1';
 
 export const CANISTORAGE_ID = (process.env.DFX_NETWORK == "ic") ? "TODO" : process.env.CANISTER_ID_CANISTORAGE;
 
+interface User {
+  identity: Secp256k1KeyIdentity;
+  principal: string;
+};
+
+function creteUserFromSingleWord(word: string): User {
+  const identity = Secp256k1KeyIdentity.fromSeedPhrase(` ${word}`.repeat(12).trim());
+  const principal = identity.getPrincipal().toText();
+  return { identity, principal };
+}
+
+// Test identities
+export const USERS:{[name:string]: User} = {
+  "admin": creteUserFromSingleWord("goddess"),
+  "test": creteUserFromSingleWord("test"),
+};
